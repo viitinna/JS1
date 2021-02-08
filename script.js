ac8762fe7e67cd34bbe56767de05d10b860f0dc0
viitinna <71917435+viitinna@users.noreply.github.com>
2021-02-08 22:52:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+let hi = "hello";
+let www = "owu";
+let ccc = "com";
+let uk = "ua";
+let xxx = 1;
+let yyy = 10;
+let min = -999;
+let od = 123;
+let drob = 3.14;
+let dr = 2.7;
+let sss = 16;
+let yes = "true";
+let no = "false";
+
+//alert(hi+www+ccc+uk+xxx+yyy+min+od+drob+dr+sss+yes+no);
+
+//document.write(hi+www+ccc+uk+xxx+yyy+min+od+drob+dr+sss+yes+no);
+
+//console.log(hi+www+ccc+uk+xxx+yyy+min+od+drob+dr+sss+yes+no);
+
+hi = "by";
+www = "wou";
+ccc = "my";
+uk = "au";
+xxx = 100;
+yyy = 1000;
+min = 99;
+od = 321;
+dr = 7.2;
+sss = 61;
+yes = "false";
+no = "true";
+
+//alert(hi+www+ccc+uk+xxx+yyy+min+od+drob+dr+sss+yes+no);
+
+//document.write(hi+www+ccc+uk+xxx+yyy+min+od+drob+dr+sss+yes+no);
+
+//console.log(hi+www+ccc+uk+xxx+yyy+min+od+drob+dr+sss+yes+no);
+
+
+
+//let userName = prompt("what is your name?");
+
+//let userSname = prompt("what is your second name?");
+
+//let userAge = prompt("how old are you");
+
+//document.write("Hello"+userName+userSname+"You are"+userAge+"years old");
+
+//let userNSnAge = prompt(userName + userSname + userAge);
+
+
+
+//let nam22 = +prompt("number 22");
+//console.log(nam22);
+
+//let nam33 = +prompt("number 33");
+//console.log(nam33);
+
+//let nam44 = +prompt("number 44");
+//console.log(nam44);
+
+//console.log(nam22+nam33+nam44);
+
+
+//let nam22 = parseInt(prompt("number 22"));
+//console.log(nam22);
+
+//let nam33 = parseInt(prompt("number 33"));
+//console.log(nam33);
+
+//let nam44 = parseInt(prompt("number 44"));
+//console.log(nam44);
+
+//let nam55 = parseInt(prompt("number 55"));
+//console.log(nam55);
+
+//console.log(nam22+nam33+nam44+nam55);
+
+
+//let nam22 = parseFloat(prompt("number 22"));
+//console.log(nam22);
+
+//let nam33 = parseFloat(prompt("number 33"));
+//console.log(nam33);
+
+//let nam44 = parseFloat(prompt("number 44"));
+//console.log(nam44);
+
+//let result = (nam22+nam33+nam44);
+//console.log(result);
+
+
+//let float = prompt("enter float");
+//let nam22 = parseFloat(float);
+//let nam33 = parseFloat(float);
+//let nam44 = parseFloat(float);
+//let round = Math.round(nam22+nam33+nam44);
+//console.log(round);
+
+
+//let nam2 = prompt("namber 2");
+//let pow2 = prompt("pow 2");
+//let result = Math.pow(nam2, pow2);
+//console.log(result);
+
+
+//let a = 100;
+//console.log(typeof a);
+
+//let b = "100";
+//console.log(typeof b);
+
+//let c = true;
+//console.log(typeof c);
+
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(5 >= 6);
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 != 10);
+console.log(10 !== 10);
+console.log(10 > 10);
+console.log(123 === "123");
+console.log(123 == "123");
